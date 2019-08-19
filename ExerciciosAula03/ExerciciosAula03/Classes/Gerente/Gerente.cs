0a7255e464e03c2deefa76b8504449e7bbbdc7e5
Leonardo Luz <44617350+LeonardoLuz2@users.noreply.github.com>
2019-08-19 12:52:08 -0300

    Add files via upload

---
+++ ExerciciosAula03/ExerciciosAula03/Classes/Gerente/Gerente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosAula03.Classes.Gerente
+{
+    public abstract class Gerente
+    {
+        public abstract void Inserir();
+        public abstract void Excluir();
+        public abstract void Alterar();
+        public abstract void Consultar();
+    }
+}
---
+++ ExerciciosAula03/ExerciciosAula03/Classes/Gerente/GerenteFornecedor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExerciciosAula03.Classes.Gerente
+{
+    public class GerenteFornecedor : Gerente
+    {
+        public void EnviarPedFornecedor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EfetuarPagFornecedor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ImprimirRelFornecedores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ImprimirRelPedFornecedores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Inserir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Excluir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Alterar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Consultar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
